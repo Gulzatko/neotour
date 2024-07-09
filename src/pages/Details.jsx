d962cff89483828ac1a5574a5d83b40ddb7e2592
Gulzatko <Mamajanovna86@gmail.com>
2024-07-09 21:20:35 +0400

    implemented the Detailed components

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -2,15 +2,14 @@ import React,{useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 
 
-function Details() {
+export default function Details() {
   const {id} = useParams();
   const [detailTrip, setDetailTrip]= useState();
    
-  useEffect
+  
 
   return (
     <div>Details</div>
   )
 }
 
-export default Details
